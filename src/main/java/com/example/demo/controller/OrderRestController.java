863283c1092242b5687d86e87207c0fc3563fafd
Talat Hussain <sethartalat@gmail.com>
2025-12-02 19:58:21 +0100

    Passing Tests for CREATE, UPDATE, and DELETE operations

--- src/main/java/com/example/demo/controller/OrderRestController.java
+++ src/main/java/com/example/demo/controller/OrderRestController.java
@@ -3,8 +3,12 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +19,31 @@ import com.example.demo.services.OrderService;
 @RequestMapping("/api/orders")
 public class OrderRestController {
 
-    @Autowired
-    private OrderService orderService;
+	@Autowired
+	private OrderService orderService;
 
-    @GetMapping
-    public List<Order> allOrders() {
-        return orderService.getAllOrders();
-    }
+	@GetMapping
+	public List<Order> allOrders() {
+		return orderService.getAllOrders();
+	}
 
-    @GetMapping("/{id}")
-    public Order order(@PathVariable long id) {
-        return orderService.getOrderById(id);  // Assuming you have this method in your service
-    }
+	@GetMapping("/{id}")
+	public Order order(@PathVariable long id) {
+		return orderService.getOrderById(id);
+	}
+
+	@PostMapping("/new")
+	public Order newOrder(@RequestBody Order order) {
+		return orderService.insertNewOrder(order);
+	}
+
+	@PutMapping("/{id}")
+	public Order updateOrder(@PathVariable long id, @RequestBody Order replacement) {
+		return orderService.updateOrderById(id, replacement);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteOrder(@PathVariable long id) {
+		orderService.deleteOrderById(id);
+	}
 }
