cc08bf74dcff85f38d525e308861379ac8cd6d93
Talat Hussain <sethartalat@gmail.com>
2025-12-02 17:25:18 +0100

    Passing GET one end-point tests

--- src/main/java/com/example/demo/controller/ProductRestController.java
+++ src/main/java/com/example/demo/controller/ProductRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,8 @@ public class ProductRestController {
         return productService.getAllProducts();
     }
 
+    @GetMapping("/{id}")
+    public Product product(@PathVariable long id) {
+        return productService.getProductById(id);
+    }
 }
