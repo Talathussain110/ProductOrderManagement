209b62f6a9f6a591709fd9a4053796570b9ae77e
Talat Hussain <sethartalat@gmail.com>
2025-12-03 19:39:40 +0100

    First passing web controller test

--- src/main/java/com/example/demo/controller/ProductWebController.java
+++ src/main/java/com/example/demo/controller/ProductWebController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProductWebController {
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
